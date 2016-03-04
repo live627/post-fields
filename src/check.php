2746550ceb60469bc4a9484e77c3ea4ef853f349
John Rayes <live627@gmail.com>
2016-03-03 22:59:47 -0700

    Update check.php

--- src/check.php
+++ src/check.php
@@ -8,7 +8,7 @@
  * @license http://opensource.org/licenses/ISC ISC
  */
  
-$required_php_version = '5.4.0';
+$required_php_version = '5.5.0';
 if (version_compare(PHP_VERSION, $required_php_version, '<')) {
 	die('Post Fields requires a minimum of PHP ' . $required_php_version . ' in order to function. (You are currently running PHP: ' . PHP_VERSION . ')');
 }
