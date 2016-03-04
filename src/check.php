11c3316cec616c6030acd7913927f09b63fffac3
John Rayes <live627@gmail.com>
2016-03-03 22:29:32 -0700

    Update check.php

--- src/check.php
+++ src/check.php
@@ -1,9 +1,13 @@
 <?php
 
 /**
- * @package CustomForms
- * @since 1.0
+ * @package PostFields
+ * @version 2.0
+ * @author John Rayes <live627@gmail.com>
+ * @copyright Copyright (c) 2011-2016, John Rayes
+ * @license http://opensource.org/licenses/ISC ISC
  */
+ 
 $required_php_version = '5.4.0';
 if (version_compare(PHP_VERSION, $required_php_version, '<')) {
 	die('Post Fields requires a minimum of PHP ' . $required_php_version . ' in order to function. (You are currently running PHP: ' . PHP_VERSION . ')');
