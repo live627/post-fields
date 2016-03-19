f193a5ea6566df8a0241e25ef93b9a0fc7845d93
John Rayes <live627@gmail.com>
2016-03-18 23:03:16 -0700

    Update Test.php

--- tests/Test.php
+++ tests/Test.php
@@ -17,7 +17,7 @@ class Test extends \PHPUnit_Framework_TestCase
     {
         $in_col = array(
             'name' => 'string', 'type' => 'string', 'size' => 'string', 'options' => 'string', 'active' => 'string', 'default_value' => 'string',
-            'can_search' => 'string', 'groups' => 'string', 'boards' => 'string', 'topic_only' => 'string', 'mi' => 'string', 'mask' => 'string',
+            'can_search' => 'string', 'groups' => 'string', 'boards' => 'string', 'topic_only' => 'string', 'bbc' => 'string', 'mask' => 'string',
         );
         $in_data = array(
             array(
